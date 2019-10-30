9a260c9e50bae6574a3ab2f6a6193cdcec521be9
Samujjwaal Dey <sdey9@uic.edu>
2019-10-30 12:59:31 -0500

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -51,8 +51,8 @@ const Scene = function(options) {
 
     self.controls = new THREE.OrbitControls( self.camera, self.renderer.domElement );
     self.controls.enableKeys = false;
-    self.controls.dampingFactor = 0.1; // friction
-    self.controls.rotateSpeed = 0.1; // mouse sensitivity
+    self.controls.dampingFactor = 0.01; // friction
+    self.controls.rotateSpeed = 0.01; // mouse sensitivity
 
     self.public = {
 
